498fe264291fc7542aa9723a6bb507034e26c8b5
amiraliks07 <amiralik.s110@gmail.com>
2025-10-23 14:24:48 +0330

    Add files via upload

---
+++ Amirk.s/ascript.js
@@ -0,0 +1,24 @@
+  function toggleMenu() {
+    document.getElementById("mul").classList.toggle("show");
+  }
+
+ function toggleNews(button) {
+    const newsBox = button.nextElementSibling;
+    newsBox.style.display = newsBox.style.display === 'block' ? 'none' : 'block';
+}
+
+// انیمیشن ظاهر شدن کارت‌ها هنگام اسکرول
+const cards = document.querySelectorAll('.card');
+
+function showCardsOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        if (cardTop < triggerBottom) {
+            card.classList.add('show');
+        }
+    });
+}
+
+window.addEventListener('scroll', showCardsOnScroll);
+window.addEventListener('load', showCardsOnScroll);
